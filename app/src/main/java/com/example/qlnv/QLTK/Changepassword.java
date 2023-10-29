18ce673e71f547085a1396d464ba010a2ee548b4
lebatinh <batinh2611@gmail.com>
2023-10-29 17:34:54 +0700

    update change password

--- app/src/main/java/com/example/qlnv/QLTK/Changepassword.java
+++ app/src/main/java/com/example/qlnv/QLTK/Changepassword.java
@@ -7,12 +7,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.qlnv.Database;
 import com.example.qlnv.R;
